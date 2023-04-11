367af54c12e231376e58486e2709a142740f1dc0
iciidev <117545902+iciidev@users.noreply.github.com>
2023-04-11 22:48:47 +0000

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,10 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Iciidev Cheats", "Midnight")
+
+
+local Main = Window:NewTab("Main")
+local MainSection = Tab:NewSection("Main")
+
+MainSection:NewButton("Back/Front Flip", "Makes you do gym", function()
+    loadstring(game:HttpGet('https://pastebin.com/raw/7wDcPtLk'))()
+end)
